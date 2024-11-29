a30941fa17d555ef43fef4c1f4d99bfe241c21d9
yoz75 <levtannn@gmail.com>
2024-11-29 14:46:54 +0300

    Added base class for entities

---
+++ Assets/Sources/Entities/Entity.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+
+namespace SpaceRoguelike
+{
+    /// <summary>
+    /// Base class for all alive-like things
+    /// </summary>
+    public abstract class Entity : MonoBehaviour
+    {
+        public float Health
+        {
+            get;
+            private set;
+        }
+
+        public float MaximalHealth
+        {
+            get;
+            private set;
+        }
+
+        public string Name
+        {
+            get;
+            protected set;
+        }
+
+        /// <summary>
+        /// Damage this entity.
+        /// Aren't you ashamed to offend this little entity!?!!??!?!
+        /// </summary>
+        /// <param name="damage"></param>
+        public void TakeDamage(float damage)
+        {
+            if(Health < damage)
+            {
+                Die();
+                return;
+            }
+            Health -= damage;
+        }
+
+        /// <summary>
+        /// Set maximal health to <paramref name="health"/>. 
+        /// If new value less than health, it will cut off health to 
+        /// new maximal health
+        /// </summary>
+        /// <param name="health">new maximal health</param>
+        protected void SetMaximalHealth(float health)
+        {
+            MaximalHealth = health;
+            if(Health > MaximalHealth)
+            {
+                Health = MaximalHealth;
+            }
+        }
+
+        public void Heal(float health)
+        {
+            Health += health;
+            if(Health > MaximalHealth)
+            {
+                Health = MaximalHealth;
+            }
+        }
+
+        /// <summary>
+        /// Great! You killed that entity! Are you happy now?
+        /// </summary>
+        protected abstract void Die();
+
+        /// <summary>
+        /// Calls when this entity collides with another entity
+        /// </summary>
+        /// <param name="other"></param>
+        protected virtual void OnCollisionWithOther(Entity other)
+        {
+            //This method is optional, so we don`t mark it with abstract
+            //and just return (otherwise the compiler will complain)
+            return;
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            var entity = collision.collider.GetComponent<Entity>();
+
+            if(entity != null)
+            {
+                OnCollisionWithOther(entity);
+            }
+        }
+    }
+}
